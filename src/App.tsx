88a884f085cd0f4ac9e0a09c5026b8a1ce9e35a8
Leda <ledathemis@protonmail.com>
2022-08-13 12:39:56 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="login" element={<Login />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
