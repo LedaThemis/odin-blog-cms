06c9f39f3ea118e1ec29e11c179795cdb180cc23
Leda <ledathemis@protonmail.com>
2022-08-14 09:53:01 +0000

    chore: remove Register route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Register from './pages/Register';
 
 function App() {
     return (
@@ -11,7 +10,6 @@ function App() {
                 <Routes>
                     <Route path="/">
                         <Route index element={<Home />}></Route>
-                        <Route path="register" element={<Register />}></Route>
                         <Route path="login" element={<Login />}></Route>
                     </Route>
                 </Routes>
